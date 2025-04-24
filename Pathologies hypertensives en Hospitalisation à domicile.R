2e886984fe84a884e4c7430944ed8f1a69d30679
Philippe MICHEL <philippe@docteur-michel.fr>
2025-04-24 15:14:55 +0200

    Bricolages.

--- Pathologies hypertensives en Hospitalisation à domicile.R
+++ Pathologies hypertensives en Hospitalisation à domicile.R
@@ -2,12 +2,36 @@
 
 #  ------------------------------------------------------------------------
 #
-# Title : 
-#    By : 
+# Title : HTA-HAD import
+#    By : PhM
 #  Date : 2025-04-18
-#    
+#
 #  ------------------------------------------------------------------------
 
+
+library(tidyverse)
+library(janitor)
+library(labelled)
+library(baseph)
+library(lubridate)
+library(readODS)
+#
+# 1. Importation des données
+#
+rm(list = ls())
+tt <- read_ods("datas/htahad.ods", sheet = "htahad", na = c("","NC")) |>
+  clean_names() |>
+  mutate(across(where(is.character), ~ as.factor(.x))) |>
+  mutate(ipp = as.factor(ipp)) |>
+  mutate(across(starts_with("date"), ~ dmy(.x))) |>
+  ## Réordonnancement de tt$terme_mise_en_place_ttt
+mutate(terme_mise_en_place_tt = fct_relevel(terme_mise_en_place_tt,
+    "arret  debut de grossesse", "avant 20SA", "apres 20SA", "apres 25SA"
+  )) |>
+  mutate(traitement = fct_relevel(traitement,
+   "aucun", "monotherapie", "bitherapie", "tritherapie"
+  )) |>
+  mutate(imc =  bmiph(imc))
 
 
 
